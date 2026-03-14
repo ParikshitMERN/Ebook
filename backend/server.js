00c1ac30ed763fd9d571ec355071e7ba85394f05
Parikshit <minitomika098@gmail.com>
2026-03-14 19:14:22 +0545

    Cors change made

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://ebook-backend-pizi.onrender.com",
+      "https://ebookify-frontend-4qfphtedf-parikshitmerns-projects.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
